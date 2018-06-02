ecc319a9e706af8935f377ca66b7335f4351a8cc
Tuan Anh Le <anhlt1983@gmail.com>
2018-06-02 09:04:18 +0700

    clear testing code of store

--- src/App.js
+++ src/App.js
@@ -41,39 +41,4 @@ function reducer(state, action) {
 const initialState = { messages: []};
 const store = createStore(reducer, initialState);
 
-// adding listener for action dispatch
-const listener = () => {
-  console.log('Current state: ' + store.getState());
-}
-// subscribe
-store.subscribe(listener);
 
-// action
-const addMessageAction1 = {
-  type: 'ADD_MESSSAGE',
-  message: 'How does it look, Neil?',
-};
-
-store.dispatch(addMessageAction1);
-const stateV1 = store.getState();
-
-const addMessageAction2 = {
-  type: 'ADD_MESSAGE',
-  message: 'Looking good',
-};
-
-store.dispatch(addMessageAction2);
-const stateV2 = store.getState();
-
-//console.log('State V1: ' + stateV1);
-//console.log('State V2: ' + stateV2);
-
-const deleteMessageAction = {
-  type: 'DELETE_MESSAGE',
-  index: 0,
-};
-
-store.dispatch(deleteMessageAction);
-const stateV3 = store.getState();
-
-//console.log('State V3: ' + stateV3);
